ba69cb9d73a4e29bd26d523acb579ec358a8527b
Ardi Nasiri <ardashir.nasirabadi@gmail.com>
2020-10-30 18:11:00 +0100

    initializing redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+import authReducer from './auth-reducer'
+import usersReducer from './users-reducer'
+import ProfileReducer from './profile-reducer'
+import errorsReducer from './errors-reducer'
+
+export default combineReducers({
+  authReducer,
+  ProfileReducer,
+  errorsReducer,
+  usersReducer,
+})
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers'
+
+const initialState = {}
+const middleware = [thunk]
+
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+  return applyMiddleware(...middleware)
+}
+
+export const store = createStore(
+  rootReducer,
+  initialState,
+  bindMiddleware(middleware)
+)
