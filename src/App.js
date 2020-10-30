157d2d68b8faa44f8a195e0249e2b156c6cc8ca1
Ardi Nasiri <ardashir.nasirabadi@gmail.com>
2020-10-30 18:00:59 +0100

    route and simple component is added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
+// Components
+import Login from './components/Login'
+import Signup from './components/Login'
+import Profile from './components/Profile'
+import Home from './components/Home'
+import Users from './components/Users'
 
 const App = () => {
   return (
-    <div>Hello world!</div>
+    <Provider store={store}>
+      <Router>
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/profile' component={Profile} />
+      <Route exact path='/users' component={Users} />
+      <Route exact path='/home' component={Home} />
+      </Router>
+    </Provider>
   )
 }
 
